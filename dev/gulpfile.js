3368f3eeafdc9a6caf7f2301ed9764e5068555ac
danilo-de-oliveira <dos.danilodeoliveira@gmail.com>
2017-06-26 13:31:25 -0300

    Change in project structure
    Change in project structure

--- dev/gulpfile.js
+++ dev/gulpfile.js
@@ -1,22 +1,31 @@
 var gulp = require('gulp'),  
 sass = require('gulp-sass'),
+usemin = require('gulp-usemin');
 sourcemaps = require('gulp-sourcemaps');
 
-var config = {  
-  srcPath: 'assets/',
-  distPath: 'dist/'
-};
- 
 gulp.task('sass', function(){  
-  return gulp.src(config.srcPath+'sass/**/*.+(scss|sass)')
+  return gulp.src('sass/**/*.+(scss|sass)')
     .pipe(sourcemaps.init())
     .pipe(sass({
       outputStyle: 'compressed'
     }).on('error', sass.logError)) // Using gulp-sass
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(config.distPath+'css'));
+    .pipe(gulp.dest('css'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch(config.srcPath+'sass/**/*.+(scss|sass)', ['sass']);
+    gulp.watch('sass/**/*.+(scss|sass)', ['sass']);
 });
+
+gulp.task('copy', function(){
+  gulp.src('images/**/*')
+    .pipe(gulp.dest('../www/images'));
+  gulp.src('fonts/**/*')
+    .pipe(gulp.dest('../www/fonts'));
+  gulp.src('css/main.css')
+    .pipe(gulp.dest('../www/css/'));
+  gulp.src('index.html')
+    .pipe(gulp.dest('../www/'));
+});
+
+gulp.task('build', ['sass', 'copy']);
